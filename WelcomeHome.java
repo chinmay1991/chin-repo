76c6a891a2f96ebf8ca63ff7cea8e97af799d0ed
Chinmaya Dehury <chinmaya.javaee@gmail.com>
2019-09-10 17:31:30 +0530

    removed commented code

--- WelcomeHome.java
+++ WelcomeHome.java
@@ -4,8 +4,5 @@ public class WelcomeHome{
   public static void main(String args[]){
     System.out.println("Welcome to Vandana Amaze");
     // comment added....
-    //again comment added.....1
-    //nnnnnn
-
   }
 }
